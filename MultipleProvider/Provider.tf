ed785f0c81033aeaf211de71ef0deb7f911fa822
sharonmathew13 <122825820+sharonmathew13@users.noreply.github.com>
2023-02-22 22:19:00 -0600

    Create Provider.tf

---
+++ MultipleProvider/Provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~> 1.3" 
+  required_providers {
+    aws = {
+        source="hashicorp/aws"
+        version="~>4.55.0"
+        }
+    }   
+  }
+
+  #provider configuration
+  provider "aws" {
+    region = "us-east-1"
+    profile = "default"
+  }
+
+  provider "aws" {
+    region = "us-west-1"
+    profile = "default"
+    alias = "us-west-1"
+    }
