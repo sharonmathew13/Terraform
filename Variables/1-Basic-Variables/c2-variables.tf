0452241cc5cd93a6a74ec2289ae5d07850aa8de9
sharonmathew13 <122825820+sharonmathew13@users.noreply.github.com>
2023-03-05 15:31:02 -0600

    Add files via upload

---
+++ Variables/1-Basic-Variables/c2-variables.tf
@@ -0,0 +1,17 @@
+# VARIABLES
+variable "aws_region" {
+  description = "The ec2 region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_ami_id" {
+  description = "THe ami id of ec2"
+  type        = string
+  default     = "ami-0dfcb1ef8550277af"
+}
+
+variable "aws_instance_count" {
+  type    = number
+  default = 1
+}
