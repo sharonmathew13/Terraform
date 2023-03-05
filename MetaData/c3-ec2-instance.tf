516aa4ee64c51bf223497a205172ebff00063533
sharonmathew13 <122825820+sharonmathew13@users.noreply.github.com>
2023-03-04 23:47:40 -0600

    Delete c3-ec2-instance.tf

--- MetaData/c3-ec2-instance.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "my-ec2-vm" {
-  ami                    = "ami-006dcf34c09e50022"
-  instance_type          = "t2.micro"
-  key_name               = "terraform-key"
-  subnet_id              = aws_subnet.vpc-dev-public-subnet.id
-  vpc_security_group_ids = [aws_security_group.vpc-dev-sg.id]
-  user_data              = file("apache-install.sh")
-
-}
